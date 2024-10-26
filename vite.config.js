bc1332f18c857680d1d7231c4fbc62b544520e5a
Minh Lu Luan <minhlu14206@gmail.com>
2024-10-26 17:52:27 +0200

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/reactApp/', // Update this to match your repository name
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000
+  }
+
 })
